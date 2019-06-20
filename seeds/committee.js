5bb83c331f9291836cea9cbaf7b68a57889f41aa
dan <daniel.bez13@gmail>
2019-06-20 14:45:23 +1200

    seed data run

--- seeds/committee.js
+++ seeds/committee.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex("table_name")
+  return knex("committee")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("table_name").insert([
+      return knex("committee").insert([
         { position: "president", member_ID: "1" },
         { position: "treasurer", member_ID: "2" },
         { position: "secretary", member_ID: "3" }
--- seeds/event_brewing.js
+++ seeds/event_brewing.js
@@ -5,9 +5,27 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex("event_brewing").insert([
-        { event_name: "brewing", paid: true, amount_owing: 20, event_role: "organiser", member_id: 1, event_id: 1 },
-        { event_name: "brewing", paid: true, amount_owing: 20, event_role: "organiser", member_id: 3, event_id: 1 }
-        { event_name: "brewing", paid: true, amount_owing: 20, event_role: "organiser", member_id: 2, event_id: 1 },
+        {
+          event_name: "brewing",
+          amount_owing: 20,
+          event_role: "organiser",
+          member_id: 1,
+          event_id: 1
+        },
+        {
+          event_name: "brewing",
+          amount_owing: 20,
+          event_role: "organiser",
+          member_id: 3,
+          event_id: 1
+        },
+        {
+          event_name: "brewing",
+          amount_owing: 20,
+          event_role: "organiser",
+          member_id: 2,
+          event_id: 1
+        }
       ]);
     });
 };
